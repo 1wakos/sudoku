92a6935d08fe7287b9531ca1b8df4f9480cd7ced
1wakos <ciapalajulka@gmail.com>
2025-04-27 15:57:23 +0200

    implement basic helpers: check row, col, box

--- main.c
+++ main.c
@@ -35,6 +35,56 @@ void printBoard(int size)
     }
 }
 
+// check if num is unused in a given row
+int unUsedInRow(int size, int row, int num)
+{
+    for (int col = 0; col < size; col++)
+    {
+        if (board[row][col] == num)
+        {
+            return 0;
+        }
+    }
+    return 1;
+}
+
+// check if num is unused in a given column
+int unUsedInCol(int size, int col, int num)
+{
+    for (int row = 0; row < size; row++)
+    {
+        if (board[row][col] == num)
+        {
+            return 0;
+        }
+    }
+    return 1;
+}
+
+// check if num is unused in a 3x3 box
+int unUsedInBox(int rowStart, int colStart, int num)
+{
+    for (int i = 0; i < 3; i++)
+    {
+        for (int j = 0; j < 3; j++)
+        {
+            if (board[rowStart + i][colStart + j] == num)
+            {
+                return 0;
+            }
+        }
+    }
+    return 1;
+}
+
+// check if it is safe to put num in board[row][col]
+int checkIfSafe(int size, int row, int col, int num)
+{
+    return unUsedInRow(size, row, num) &&
+           unUsedInCol(size, col, num) &&
+           unUsedInBox(row - row % 3, col - col % 3, num);
+}
+
 int main()
 {
     printf("sudoku\n");
